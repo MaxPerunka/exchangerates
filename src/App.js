e7280d4fffffcd4de73701a7fe538c0b08811e50
MaxPerunka <n2pema00@students.oamk.fi>
2023-02-27 18:31:50 +0200

    declared variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 const URL= 'https://api.exchangerate.host/latest'
 
 function App() {
+  const [eur,setEur] = useState(0);
+  const [gbp,setGbp] = useState(0);
+  const [rate,setRate] = useState(0);
+
   return (
     <div id="container">
 
